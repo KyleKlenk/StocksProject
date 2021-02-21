8099c9906ff29f7ea773905fa00a5a7c245f21f4
Kyle Klenk <kyle.c.klenk@gmail.com>
2021-02-21 14:03:48 -0600

    can show stock prices on tkinter widget

--- apiTest.py
+++ apiTest.py
@@ -5,13 +5,58 @@ from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
 
+
 style.use('ggplot')
-start = dt.datetime(2021,2,19)
-end = dt.datetime(2021,2,19)
+### Global Variables ###
+todaysDate = dt.datetime(dt.date.today().year, 
+                        dt.date.today().month, 
+                        dt.date.today().day)
 
-while(1):
+"""
+Input: TickerSymbol and TickerContainerStructure
+Output: TickerContainer with new TickerSymbol
+"""
+def addStockTicker(ticker, tickerSymbolsList):
+    tickerSymbolsList.append(ticker)
+    return tickerSymbolsList
 
-    df = web.DataReader('SNC.TO', 'yahoo', start, end)
-    print(df.head())
-    time.sleep(60)
+"""
+Input: TickerSymbol and TickerContainerStructure
+Output: TickerContainer without TickerSymbol
+"""
+def removeStockTicker(ticker, tickerSymbolsList):
+    tickerSymbolsList.remove(ticker)
+    return tickerSymbolsList
+
+"""
+Input: dictionary to hold the key value pairs
+Key = Ticker Symbol
+Value = DataFrame object
+"""
+def getStockPrices(dataStructure, tickerSymbolsList):
+# date will be an error when stock market is not open
+    for ticker in tickerSymbolsList:
+        df = web.DataReader(ticker, 'yahoo', dt.datetime(dt.date.today().year, dt.date.today().month, dt.date.today().day - 2), 
+        dt.datetime(dt.date.today().year, dt.date.today().month, dt.date.today().day - 2))
+        column = df.iloc[0]["Close"]
+        print(column)
+        dataStructure[ticker] = column
+    return dataStructure
+
+
+def main():
+    myPortfolioTickers = []
+    myDataframes = {}
+
+    myPortfolioTickers = addStockTicker("SNC.TO", myPortfolioTickers)
+    myDataFrames = getStockPrices(myDataframes, myPortfolioTickers)
+
+    
+
+
+
+# while(1):
+#      df = web.DataReader('SNC.TO', 'yahoo', todaysDate, todaysDate)
+#      print(df.head())
+#      time.sleep(60)
 
