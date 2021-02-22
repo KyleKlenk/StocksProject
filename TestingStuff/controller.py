039518667920a0e4896603fe6abe13cea91f1c1e
Kyle Klenk <kyle.c.klenk@gmail.com>
2021-02-22 11:38:02 -0600

    clean up

--- TestingStuff/controller.py
+++
@@ -1,9 +0,0 @@
-class Controller:
-    def __init__(self, model):
-        self.model = model
-    
-    def addTickerClick(self, text):
-        print(text)
-
-    def removeTickerClick(self, text):
-        print(text)
